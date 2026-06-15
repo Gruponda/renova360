116474c3f24d9d7f1a8df293a44f2aacf2061077
Gruponda <reformasnda@gmail.com>
2026-06-15 12:08:33 +0200

    svhimb

---
+++ stackbit.config.ts
@@ -0,0 +1,22 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0", // Añade esta línea para evitar errores de versión
+  ssgName: "custom",         // Añade esta línea para indicarle que es tu propio React
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
+});
